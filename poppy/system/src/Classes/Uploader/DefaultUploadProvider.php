43849f4cb8463c1d130258d3c65f27407509e5cc
Axerli <kiratst@outlook.com>
2022-09-16 10:43:29 +0800

    fix: 修复多图上传图片地址相同问题

--- poppy/system/src/Classes/Uploader/DefaultUploadProvider.php
+++ poppy/system/src/Classes/Uploader/DefaultUploadProvider.php
@@ -360,7 +360,6 @@ class DefaultUploadProvider implements UploadContract
             if ($ext !== $extension) {
                 throw new ApplicationException('指定文件的扩展类型不符, 可能导致图片无法展示');
             }
-            return $this->destination;
         }
         $now      = Carbon::now();
         $fileName = $now->format('is') . Str::random(8) . '.' . $extension;
