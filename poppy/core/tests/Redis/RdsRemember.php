11683ae08eadfa4970a6c44df3ae840befe9b38c
多厘 <zhaody901@126.com>
2023-01-04 13:53:02 +0800

    fix: remember rds error

--- poppy/core/tests/Redis/RdsRemember.php
+++ poppy/core/tests/Redis/RdsRemember.php
@@ -9,10 +9,17 @@ class RdsRemember extends RdsBaseTest
 
     public function testRemember()
     {
+
         $value = $this->rds->remember('remember', 20, function () {
             return 4;
         });
+        // get from cache
+        $secondValue = $this->rds->remember('remember', 20, function () {
+            return 5;
+        });
         $this->assertEquals(4, $value);
+        $this->assertEquals(4, $secondValue);
+        $this->rds->del('remember');
 
 
         $value = $this->rds->remember('remember-forever', 0, function () {
