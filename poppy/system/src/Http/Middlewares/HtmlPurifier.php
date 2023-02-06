b3a241eb1db49d55acc6867abdecddf0af5b8e15
zhangxin <734228006@qq.com>
2023-02-06 18:18:44 +0800

    feat: html过滤中间件(sys-html_purifier)单元测试

--- poppy/system/src/Http/Middlewares/HtmlPurifier.php
+++ poppy/system/src/Http/Middlewares/HtmlPurifier.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Poppy\System\Http\Middlewares;
 
 use Closure;
+use File;
+use HTMLPurifier_Config;
 use Illuminate\Http\Request;
+use Storage;
 
 /**
  * Html净化
@@ -14,8 +17,13 @@ class HtmlPurifier
 {
     public function handle(Request $request, Closure $next)
     {
-        $config = \HTMLPurifier_Config::createDefault();
-        $config->set('Cache.SerializerPath', storage_path());
+        $Storage = Storage::disk('storage');
+        $cachePath = $Storage->path('html_purifier/');
+        if (!File::exists($cachePath)){
+            File::makeDirectory($cachePath);
+        }
+        $config = HTMLPurifier_Config::createDefault();
+        $config->set('Cache.SerializerPath', $cachePath);
         $Purifier = new \HTMLPurifier($config);
         $input = $request->all();
         array_walk_recursive($input, static function (&$input) use ($Purifier) {
---
+++ poppy/system/tests/Action/HtmlPurifierTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Poppy\System\Tests\Action;
+
+use File;
+use HTMLPurifier;
+use HTMLPurifier_Config;
+use Poppy\Framework\Application\TestCase;
+use Storage;
+
+class HtmlPurifierTest extends TestCase
+{
+    /**
+     * html净化
+     * @return void
+     */
+    public function testPurifier(): void
+    {
+        $old ='
+        <H1>我是一级标题</H1>
+        <h2>我是二级标题</h2>
+        <h3 style="color: red">我是红色的三级标题</h3>
+        <iframe src="http://baidu.com" frameborder="0"></iframe>
+        <script>alert("alert")</script>
+        <sCRiPt sRC=http://baidu.com></sCrIpT>
+        <style>h1{color: red}</style>
+        ';
+        $Storage = Storage::disk('storage');
+        $cachePath = $Storage->path('html_purifier/');
+        if (!File::exists($cachePath)){
+            File::makeDirectory($cachePath);
+        }
+        $config = HTMLPurifier_Config::createDefault();
+        $config->set('Cache.SerializerPath', $cachePath);
+        $Purifier = new HTMLPurifier($config);
+        $new = $Purifier->purify($old);
+        print_r($old);
+        print_r($new);
+    }
+}
