394f97b0f8886cdea2ad561c6179d2631fe6d873
zhangxin <734228006@qq.com>
2023-02-06 16:42:35 +0800

    feat: html过滤中间件(sys-html_purifier)更改缓存位置

--- poppy/system/src/Http/Middlewares/HtmlPurifier.php
+++ poppy/system/src/Http/Middlewares/HtmlPurifier.php
@@ -14,9 +14,12 @@ class HtmlPurifier
 {
     public function handle(Request $request, Closure $next)
     {
+        $config = \HTMLPurifier_Config::createDefault();
+        $config->set('Cache.SerializerPath', storage_path());
+        $Purifier = new \HTMLPurifier($config);
         $input = $request->all();
-        array_walk_recursive($input, static function (&$input) {
-            $input = (new \HTMLPurifier())->purify($input);
+        array_walk_recursive($input, static function (&$input) use ($Purifier) {
+            $input = $Purifier->purify($input);
         });
         $request->merge($input);
         return $next($request);
