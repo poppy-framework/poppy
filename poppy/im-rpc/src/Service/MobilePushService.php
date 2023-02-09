e7ea1967d4f2caac6085b7329aeb7a9778c20822
Axerli <kiratst@outlook.com>
2023-02-09 10:55:28 +0800

    feat: im-rpc push unbind

--- poppy/im-rpc/src/Service/MobilePushService.php
+++ poppy/im-rpc/src/Service/MobilePushService.php
@@ -7,6 +7,7 @@ namespace Poppy\Im\Rpc\Service;
 use Poppy\Im\Rpc\Value\From;
 use Poppy\Im\Rpc\Value\Message;
 use Poppy\Im\Rpc\Value\Packer;
+use Poppy\Im\Rpc\Value\RegisterDevice;
 use Poppy\Im\Rpc\Value\Team;
 
 /**
@@ -17,13 +18,34 @@ interface MobilePushService
 {
     /**
      * 注册阿里推送
-     *
-     * @param array $data
+     * @param RegisterDevice $device
      * @return bool
      */
-    public function register(array $data): bool;
+    public function register(RegisterDevice $device): bool;
 
+    /**
+     * 取消ali推送的绑定
+     * @param string $uid
+     * @param array  $pushIds
+     * @return bool
+     */
+    public function unbind(string $uid, array $pushIds): bool;
+
+    /**
+     * 通知信息
+     * @param From    $from
+     * @param Team    $team
+     * @param Message $message
+     * @return Packer
+     */
     public function notification(From $from, Team $team, Message $message): Packer;
 
+    /**
+     * 发送推送
+     * @param Packer $notificationPacker
+     * @param Team   $team
+     * @param array  $targetUid
+     * @return void
+     */
     public function push(Packer $notificationPacker, Team $team, array $targetUid): void;
 }
