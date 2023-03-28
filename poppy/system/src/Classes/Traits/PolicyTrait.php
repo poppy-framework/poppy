56adf9c381da0252177599548d5c3edf791a2650
duoli <zhaody901@126.com>
2023-03-28 21:03:01 +0800

    feat: 权限进行后置

--- poppy/system/src/Classes/Traits/PolicyTrait.php
+++ poppy/system/src/Classes/Traits/PolicyTrait.php
@@ -13,12 +13,12 @@ trait PolicyTrait
 {
 
     /**
-     * 在 XX 条件的前置, 用于合并权限
+     * 在 XX 条件的后置, 用于进行权限的后置限制
      * @param PamAccount $pam     账号
      * @param string     $ability 能力
      * @return bool|null
      */
-    public function before(PamAccount $pam, string $ability): ?bool
+    public function after(PamAccount $pam, string $ability): ?bool
     {
         $permission = self::$permissionMap[$ability] ?? '';
         return $permission ? $pam->capable($permission) : null;
