ca7d25fb6d7e48c903211dbc3bab40b2ed81a20d
Axerli <kiratst@outlook.com>
2023-05-09 11:18:32 +0800

    feat: socket push service

---
+++ poppy/im-rpc/src/Service/SocketPushService.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace Poppy\Im\Rpc\Service;
+
+interface SocketPushService
+{
+    /**
+     * 广播
+     * @param array  $data      推送数据
+     * @param string $event     event
+     * @param string $namespace nsp
+     * @return mixed
+     */
+    public function broadcast(array $data, string $event, string $namespace = '');
+
+    /**
+     * 发送信息
+     * @param string $event event
+     * @param array  $users 目标用户
+     * @param array  $data  发送信息
+     * @return mixed
+     */
+    public function emit(string $event, array $users, array $data);
+
+    /**
+     * 是否有客户端
+     * @param string $uid
+     * @return bool
+     */
+    public function hasClient(string $uid): bool;
+}
