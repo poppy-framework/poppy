8b212efdd28b1dedf2d0de1ec3e48e829b67b597
duoli <zhaody901@126.com>
2023-07-13 17:56:45 +0800

    feat: 移除 fontawesome 字体

--- modules/demo/resources/views/web/home/index.blade.php
+++ modules/demo/resources/views/web/home/index.blade.php
@@ -2,52 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Poppy Framework V4 Demo</title>
     <style>
-    .main {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        justify-items: center;
-        flex-direction: column;
-        min-height: 100vh;
-        color: #ccc;
-    }
+        .main {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            justify-items: center;
+            flex-direction: column;
+            min-height: 100vh;
+            color: #ccc;
+        }
 
-    .main h3 {
-        font-weight: lighter;
-    }
+        .main h3 {
+            font-weight: lighter;
+        }
 
-    .main a {
-        color: #ccc;
-        padding: 3px 6px;
-        font-weight: lighter;
-        text-decoration: none;
-        border-bottom: 1px solid #ccc;
-        display: inline-block;
-        margin: 0.5rem 1rem;
-    }
+        .main a {
+            color: #ccc;
+            padding: 3px 6px;
+            font-weight: lighter;
+            text-decoration: none;
+            border-bottom: 1px solid #ccc;
+            display: inline-block;
+            margin: 0.5rem 1rem;
+        }
 
-    .main small a {
-        color: #ccc;
-        padding: 3px 6px;
-        font-weight: lighter;
-        text-decoration: none;
-        border-bottom: none;
-    }
+        .main small a {
+            color: #ccc;
+            padding: 3px 6px;
+            font-weight: lighter;
+            text-decoration: none;
+            border-bottom: none;
+        }
     </style>
 </head>
 <body>
 <div class="main">
     <h3>Poppy Framework V4 Demo</h3>
     <p>
-        <a href="/docs/php">PHP 文档</a>
-        <a href="/docs/web">Api 文档</a>
-        <a href="https://wulicode.com/doc/v4">使用手册</a>
+        <a target="_blank" href="/docs/php">PHP 文档</a>
+        <a target="_blank" href="/docs/web">Api 文档</a>
+        <a target="_blank" href="https://weiran.tech">使用手册</a>
     </p>
-    <small>powered by <a href="https://github.com/dadi-cn" target="_blank">dadi-cn</a></small>
+    <small>powered by <a href="https://docs.huowanes.com" target="_blank">dadi-cn</a></small>
 </div>
 </body>
 </html>
