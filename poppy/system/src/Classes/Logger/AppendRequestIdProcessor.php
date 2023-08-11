b3cf347f316ef58425a29bd69e47e1e8db7f9bed
Axerli <kiratst@outlook.com>
2023-08-11 15:20:35 +0800

    feat: append request id generate

--- poppy/system/src/Classes/Logger/AppendRequestIdProcessor.php
+++ poppy/system/src/Classes/Logger/AppendRequestIdProcessor.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Poppy\System\Classes\Logger;
 
+use Illuminate\Support\Str;
 use Monolog\Processor\ProcessorInterface;
 
 class AppendRequestIdProcessor implements ProcessorInterface
@@ -12,6 +13,12 @@ class AppendRequestIdProcessor implements ProcessorInterface
     public function __invoke(array $record)
     {
         $requestId = request()->requestId ?? '';
+
+        if (!$requestId) {
+            $requestId = Str::uuid()->toString();
+
+            request()->requestId = $requestId;
+        }
 
         $record['extra']['request_id'] = $requestId;
 
