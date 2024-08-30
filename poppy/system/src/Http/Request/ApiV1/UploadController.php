608b71dc71466d458b12f89456acf4e2cdee3c36
mowangjuanzi <baoguoxiao0538@hotmail.com>
2024-08-30 14:18:59 +0800

    feat: 修复 bug

--- poppy/system/src/Http/Request/ApiV1/UploadController.php
+++ poppy/system/src/Http/Request/ApiV1/UploadController.php
@@ -104,7 +104,7 @@ class UploadController extends JwtApiController
                 // 如果是图片，则通过 mime 再次进行检测
                 $extension = strtolower($_img->getClientOriginalExtension());
                 if (isset(self::ALLOW_IMAGE_MIME[$extension]) && $_img->getMimeType() !== self::ALLOW_IMAGE_MIME[$extension]) {
-                    return '只允许上传 "' . implode(',', self::ALLOW_FILE_EXTENSIONS) . '" 格式';
+                    return Resp::error('只允许上传 "' . implode(',', self::ALLOW_FILE_EXTENSIONS) . '" 格式');
                 }
 
                 if ($Image->saveFile($_img)) {
