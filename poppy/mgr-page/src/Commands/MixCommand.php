12f4e68121b0793bc16f7a2b5cfb29e30b5db166
duoli <zhaody901@gmail.com>
2026-07-21 21:49:47 +0800

    feat: move js to npm support

--- poppy/mgr-page/src/Commands/MixCommand.php
+++ poppy/mgr-page/src/Commands/MixCommand.php
@@ -6,6 +6,7 @@ namespace Poppy\MgrPage\Commands;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Finder\SplFileInfo;
+use Throwable;
 
 class MixCommand extends Command
 {
@@ -34,6 +35,14 @@ class MixCommand extends Command
                 'node_modules/bootstrap-icons/font/bootstrap-icons.scss'        => 'poppy/mgr-page/resources/style/bootstrap-icons/bootstrap-icons.scss',
                 'node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff'  => 'poppy/mgr-page/resources/font/bootstrap-icons/bootstrap-icons.woff',
                 'node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2' => 'poppy/mgr-page/resources/font/bootstrap-icons/bootstrap-icons.woff2',
+                'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'        => 'poppy/mgr-page/resources/libs/bootstrap@5.3/bootstrap.bundle.min.js',
+                'node_modules/easytimer.js/dist/easytimer.min.js'               => 'poppy/mgr-page/resources/libs/easytimer/easytimer.min.js',
+                'node_modules/clipboard/dist/clipboard.min.js'                  => 'poppy/mgr-page/resources/libs/clipboard/clipboard.min.js',
+                'node_modules/js-cookie/dist/js.cookie.min.js'                  => 'poppy/mgr-page/resources/libs/js-cookie/js.cookie.min.js',
+                'node_modules/js-storage/js.storage.min.js'                     => 'poppy/mgr-page/resources/libs/js-storage/js.storage.min.js',
+                'node_modules/lodash/lodash.min.js'                             => 'poppy/mgr-page/resources/libs/lodash/lodash.min.js',
+                'node_modules/tom-select/dist/js/tom-select.complete.min.js'    => 'poppy/mgr-page/resources/libs/tom-select/tom-select.complete.min.js',
+                'node_modules/tom-select/dist/css/tom-select.css'               => 'poppy/mgr-page/resources/libs/tom-select/tom-select.css',
             ];
             collect($files)->each(function ($aim, $ori) {
                 app('files')->ensureDirectoryExists(dirname(base_path($aim)));
@@ -43,6 +52,7 @@ class MixCommand extends Command
 
             $folders = [
                 'node_modules/bootstrap/scss' => 'poppy/mgr-page/resources/style/bootstrap@5.3',
+                'node_modules/layui/dist'     => 'poppy/mgr-page/resources/libs/layui',
             ];
             collect($folders)->each(function ($aim, $ori) {
                 app('files')->copyDirectory(base_path($ori), base_path($aim));
@@ -57,10 +67,15 @@ class MixCommand extends Command
             collect($swaggerUiFiles)->each(function (SplFileInfo $ori) use ($swaggerUiAim) {
                 if (in_array($ori->getExtension(), ['css', 'js', 'html'])) {
                     if ('swagger-initializer.js' === $ori->getFilename()) {
-                        $basePath        = base_path($swaggerUiAim . $ori->getFilename());
-                        $content         = app('files')->get($ori->getRealPath());
-                        $replacedContent = str_replace('https://petstore.swagger.io/v2/swagger.json', './weiran.json', $content);
-                        app('files')->put($basePath, $replacedContent);
+                        $basePath = base_path($swaggerUiAim . $ori->getFilename());
+                        try {
+                            $content         = app('files')->get($ori->getRealPath());
+                            $replacedContent = str_replace('https://petstore.swagger.io/v2/swagger.json', './weiran.json', $content);
+                            app('files')->put($basePath, $replacedContent);
+                        }
+                        catch (Throwable $e) {
+                            $this->error(sys_gen_mk(self::class, "Read {$ori->getRealPath()} failed: {$e->getMessage()}"));
+                        }
                     }
                     else {
                         app('files')->copy($ori->getRealPath(), base_path($swaggerUiAim . $ori->getFilename()));
