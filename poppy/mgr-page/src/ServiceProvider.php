6a7a8c4e116df4ce44cf4fc278560fbda229a97a
duoli <zhaody901@gmail.com>
2026-07-23 15:38:14 +0800

    chore: 移除 jshash 资源发布配置

--- poppy/mgr-page/src/ServiceProvider.php
+++ poppy/mgr-page/src/ServiceProvider.php
@@ -39,7 +39,6 @@ class ServiceProvider extends PoppyServiceProvider
             // 编辑器
             __DIR__ . '/../resources/libs/jquery/backstretch'     => public_path('assets/libs/jquery/backstretch'),
             __DIR__ . '/../resources/libs/vue'                    => public_path('assets/libs/vue'),
-            __DIR__ . '/../resources/libs/jshash'                 => public_path('assets/libs/jshash'),
             __DIR__ . '/../resources/libs/easy-web/'              => public_path('assets/libs/easy-web/'),
             __DIR__ . '/../resources/libs/jquery/data-tables/'    => public_path('assets/libs/jquery/data-tables/'),
             __DIR__ . '/../resources/libs/layui/'                 => public_path('assets/libs/layui/'),
